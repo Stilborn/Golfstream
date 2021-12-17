d290dee2ef3311d45d6b7db608af877990d17dc7
Vsevolod <61139472+Stilborn@users.noreply.github.com>
2021-12-17 18:46:47 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,4 +4,8 @@ $(function () {
 		$('.menu-show').toggleClass('order');
 		$('.nav__list').toggleClass('menu-showed');
 	});
+	//Create-select
+	$(".cr-select").click(function(){				
+		$('.crct'+($(this).val())).toggleClass('d-none d-flex');
+	});
 })

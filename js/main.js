c1c8d15b31869831ca8557128ea62da53dddf278
Vsevolod <61139472+Stilborn@users.noreply.github.com>
2021-12-20 16:24:52 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,17 @@ $(function () {
 		$('.menu-show').toggleClass('order');
 		$('.nav__list').toggleClass('menu-showed');
 	});
+	//ModalWindows
+		//Record
+	$('.record').click(function(e){
+		e.preventDefault();
+		$('.form-record').arcticmodal();
+	});
+		//Test
+	$('.test-btn').click(function(e){
+		e.preventDefault();
+		$('.form-test').arcticmodal();
+	});
 	//Create-select
 	$(".cr-select").click(function(){				
 		$('.crct'+($(this).val())).toggleClass('d-none d-flex');
